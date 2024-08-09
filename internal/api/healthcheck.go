da0b28bbbeb00402b45d5ef37f69efb67affafec
dv4mp1r3 <somebody@somewhere>
2024-08-10 00:22:54 +0300

    1: typo fix

--- internal/api/healthcheck.go
+++ internal/api/healthcheck.go
@@ -7,7 +7,7 @@ import (
 	"github.com/GORATOR/backend/internal/utils"
 )
 
-func Healscheck(w http.ResponseWriter, r *http.Request) {
+func Healthscheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	model := models.Healthcheck{Version: "1", Status: "OK", PostgresVersion: "16"}
 	utils.HttpReturnJson(w, model)
