552edfff17fed771e6caf68481bd3d7f22ff98fc
dv4mp1r3 <somebody@somewhere>
2024-08-10 17:02:22 +0300

    added app config prototype

--- internal/api/healthcheck.go
+++ internal/api/healthcheck.go
@@ -10,10 +10,17 @@ import (
 
 func Healthscheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+	pgVersion := database.GetDatabaseVersion()
+	var status string
+	if pgVersion == "" {
+		status = "Database connection isn't active"
+	} else {
+		status = "OK"
+	}
 	model := models.Healthcheck{
 		Version:         "1",
-		Status:          "OK",
-		PostgresVersion: database.GetDatabaseVersion(),
+		Status:          status,
+		PostgresVersion: pgVersion,
 	}
 	utils.HttpReturnJson(w, model)
 }
