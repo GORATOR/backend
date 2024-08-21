493a7e3954887d5920d0171dca26ea3840e3d699
dv4mp1r3 <somebody@somewhere>
2024-08-21 23:31:26 +0300

    only post method allowed

--- internal/api/envelope.go
+++ internal/api/envelope.go
@@ -11,6 +11,11 @@ import (
 )
 
 func Envelope(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		envelopeBadRequest(w)
